0f86efaaab02720d1d276917fadc3f50c02dee4e
duartefalmeida <34278054+duartefalmeida@users.noreply.github.com>
2019-05-02 21:55:43 +0100

    test

--- SD2019-Microgram-Code/testn-sd1.sh
+++ SD2019-Microgram-Code/testn-sd1.sh
@@ -15,7 +15,7 @@ mkdir -p $LOGS
 
 #update the images, in particular the tester 
 docker pull smduarte/sd19-tp1-tester
-#docker pull $2
+docker pull $2
 
 #execute the client with the given command line parameters
 docker run --network=sd-net -it -v $LOGS:/logs/ -v /var/run/docker.sock:/var/run/docker.sock smduarte/sd19-tp1-tester:latest $*
